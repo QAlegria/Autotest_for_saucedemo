47b71a6db0a50d7eb83f2583ad7a40800beaf109
QAlegria <test.space.job@gmail.com>
2025-08-04 15:54:42 +0700

    Add pytest_runtest_makereport to make screenshoots, when tests failed

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from playwright.sync_api import Page
 
@@ -134,3 +135,14 @@ def get_image_api():
 def prod_list():
     return []
 
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        page = item.funcargs.get("page", None)
+        if page:
+            screenshot = page.screenshot()
+            allure.attach(screenshot, name="screenshot", attachment_type=allure.attachment_type.PNG)
